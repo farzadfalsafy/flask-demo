46a2543fd76f18670bf8f438acc8f8f70ea5be15
Farzad Falsafi <farzad.falsafi@outlook.com>
2021-01-28 05:17:53 +0000

    adding local files for Flask app

---
+++ main.py
@@ -0,0 +1,12 @@
+
+from flask import Flask, escape, request
+app=Flask(__name__)
+@app.route('/')
+def hello():
+    name=request.args.get("name","world")
+    return f'hello, {escape(name)}!'
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
